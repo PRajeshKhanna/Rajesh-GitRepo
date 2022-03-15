aeacbee5d70dc81e3190f3f7eb4ade7ac977743f
PRajeshKhanna <101185257+PRajeshKhanna@users.noreply.github.com>
2022-03-15 21:19:08 +0530

    Add files via upload

---
+++ Web App Deployment With Pre-Defined Variables.ps1
@@ -0,0 +1,26 @@
+﻿# This PS Script Will Deploy A Azure WebApp BY Using The Variables Pre Defined.
+
+<# Declaring A VAriable In PowerShell
+   $variablename. #>
+
+
+<# InOrder To Create A WEBAPP We Need The Following Information
+   1)Resource Group Name,
+   2)Webapp Service Name,
+   3)WebApp Location Name,
+   4)WebApp ASP Name. #>
+
+   $RGname = "RajeshRG"
+
+   $WebAppname = "RajeshWebApp"
+
+   $WebAppLocation = "centralus"
+
+   $WebAppASPname = "RajeshASP"
+
+# Procedure To Follow To Create A WebApp With Variables.
+
+
+# New-AzWebApp -ResourceGroupName $RGname -Name $WebAppname -Location $WebAppLocation -AppServicePlan $WebAppASPname -Verbose
+
+  Remove-AzWebApp -ResourceGroupName $RGname -Name $WebAppname -Verbose
